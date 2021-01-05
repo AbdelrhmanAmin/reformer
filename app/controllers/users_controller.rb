f9538b720d655b6528e0ed04bcf7527a58db74ea
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-05 22:05:47 +0200

    changing requirments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to new_user_path
     else
       render :new
     end
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to root_path
+      redirect_to edit_user_path
     else
       render :edit
     end
